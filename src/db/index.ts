17214d0efafbd456ff017472b72fbbba9c4ae03d
Biella Mariscotes <i_biellamae.mariscotes@stratpoint.com>
2025-06-29 17:09:02 +0800

    docs: removed unnecessary comment

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,6 +7,5 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not defined in .env.local");
 }
 
-// Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(connectionString, { prepare: false });
 export const db = drizzle(client);
