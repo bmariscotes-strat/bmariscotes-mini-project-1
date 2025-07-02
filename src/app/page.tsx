348eae7d743c479408abd164d3464f9f5b2865b2
Biella Mariscotes <i_biellamae.mariscotes@stratpoint.com>
2025-07-02 11:36:32 +0800

    fix(home): fixed eslint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
       <section className="intro h-screen overflow-hidden">
         <div className="flex flex-col text-container">
           <Image src="/brand/logo-w.png" className="logo" alt="Logo" width={1588} height={1284} />
-          <p className="tag-line pt-10 text-white">Hello, I'm</p>
+          <p className="tag-line pt-10 text-white">Hello, {`I'm`}</p>
           <p className="text-9xl text-white">Biella!</p>
 
           <p className="desc text-white pt-10">A web developer and UI/UX designer.</p>
